7f92269a7469f8fb8e28309f8bf521df441d3508
Madhusmita47 <115548760+Madhusmita47@users.noreply.github.com>
2023-01-11 12:02:04 +0530

    2 files modified

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -14,8 +14,8 @@ export default function App() {
       </div>
         </div>
       
-      
         <img className="p" src="https://o.remove.bg/downloads/cc5ed6b6-d657-4797-b1f0-a4944ddce8c5/image-removebg-preview.png"/>
+      
       <div className="p">
         <p ><b>Here you can find the best books..to enhance your skills."keep reading",its one of the most marvelous adventures that anyone can have..!!</b></p>
         </div>
